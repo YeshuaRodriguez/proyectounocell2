7b25e3134031300c2b557d86b9250e8d428f8103
felix-alvarado14 <alvarado.felix00789@gmail.com>
2025-04-09 02:33:49 -0600

    commit prueba handlebar servicios index

--- data/pages/indexPage.js
+++ data/pages/indexPage.js
@@ -1,3 +1,22 @@
 export const indexPage = {
-    
+    indexServices:[
+        {
+            servicio: "Últimos Celulares",
+            servicioDesc: "Explora los modelos más recientes y avanzados en el mercado.",
+            imgSrc: "https://cdn-icons-png.flaticon.com/256/0/191.png",
+            imgDesc: "Celulares"
+        },
+        {
+            servicio: "Accesorios",
+            servicioDesc: "Encuentra los mejores accesorios para tu dispositivo.",
+            imgSrc: "https://cdn-icons-png.flaticon.com/256/3205/3205414.png",
+            imgDesc: "Accesorios"
+        },
+        {
+            servicio: "Reparaciones",
+            servicioDesc: "Ofrecemos servicios de reparación para celulares, televisores y otros dispositivos, incluyendo estufas.",
+            imgSrc: "https://cdn-icons-png.flaticon.com/256/675/675507.png",
+            imgDesc: "Reparación"
+        }
+    ]
 }
