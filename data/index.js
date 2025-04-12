ae3c51952b675c9200e0d4124283323a0ec81ed7
felix-alvarado14 <alvarado.felix00789@gmail.com>
2025-04-11 21:36:55 -0600

    cambios index.js

--- data/index.js
+++ data/index.js
@@ -7,10 +7,12 @@ const commonData = {
         { url: '/about.html', label: 'Acerca de Nosotros' },
         { url: '/contact.html', label: 'Contáctanos' },
         { url: '/donations.html', label: 'Donaciones' },
+        { url: '/events.html', label: 'Eventos'},
         { url: '/faq.html', label: 'FAQ' },
         { url: '/gallery.html', label: 'Galería' },
         { url: '/help.html', label: 'Ayuda' },
-        { url: '/programs.html', label: 'Programas' }
+        { url: '/programs.html', label: 'Programas' },
+        { url: '/blog.html', label: 'Blog' },
     ]
 }
 
