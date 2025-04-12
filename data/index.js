2034c4dd4d177458bb9e18da8d733fa8cff53f22
felix-alvarado14 <alvarado.felix00789@gmail.com>
2025-04-12 03:12:10 -0600

    index.js rutas modificadas

--- data/index.js
+++ data/index.js
@@ -3,16 +3,16 @@ import { indexPage } from "./pages/indexPage";
 
 const commonData = {
     navigation: [
-        { url: '/index.html', label: 'Inicio' },
-        { url: '/about.html', label: 'Acerca de Nosotros' },
-        { url: '/contact.html', label: 'Contáctanos' },
-        { url: '/donations.html', label: 'Donaciones' },
-        { url: '/events.html', label: 'Beneficios'},
-        { url: '/faq.html', label: 'FAQ' },
-        { url: '/gallery.html', label: 'Localizaciones' },
-        { url: '/help.html', label: 'Cómo Donar' },
-        { url: '/programs.html', label: 'Programas' },
-        { url: '/blog.html', label: 'Blog' },
+        { url: './index.html', label: 'Inicio' },
+        { url: './about.html', label: 'Acerca de Nosotros' },
+        { url: './contact.html', label: 'Contáctanos' },
+        { url: './donations.html', label: 'Donaciones' },
+        { url: './events.html', label: 'Beneficios'},
+        { url: './faq.html', label: 'FAQ' },
+        { url: './gallery.html', label: 'Localizaciones' },
+        { url: './help.html', label: 'Cómo Donar' },
+        { url: './programs.html', label: 'Programas' },
+        { url: './blog.html', label: 'Blog' },
     ]
 }
 
